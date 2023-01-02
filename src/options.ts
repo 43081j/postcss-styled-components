640aed6a961fb7737fcda4bd323bab75a79d5bcc
43081j <43081j@users.noreply.github.com>
2023-01-02 15:31:51 +0000

    feat: support css templates

--- src/options.ts
+++ src/options.ts
@@ -2,5 +2,5 @@ import {SyntaxOptions} from 'postcss-js-core';
 
 export const options: SyntaxOptions = {
   id: 'styled-components',
-  tagNames: ['styled', 'styled.*', 'styled(*', 'createGlobalStyle']
+  tagNames: ['styled', 'styled.*', 'styled(*', 'createGlobalStyle', 'css']
 };
